8da2ec83d9bde369d9b632638dbe439202d91fb6
Ivan Mikhailov <mikhailov.ivan@gmail.com>
2023-02-02 14:48:48 +0300

    Added thermostat

--- MM.py
+++ MM.py
@@ -21,21 +21,23 @@ class ForceField():
     
 
 class Box():
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
     def periodic_correct(self, xb, yb):
         return periodic(xb, self.x), periodic(yb, self.y)
         
 class Bead():
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.v_x = 0.0
-        self.v_y = 0.0
+        self.v_x = np.random.uniform(-2.0, 2.0)
+        self.v_y = np.random.uniform(-2.0, 2.0)
         self.a_x = 0.0
         self.a_y = 0.0
-    def move_bead(self,dt): # ff
+    def move_bead(self, dt): # ff
+        self.v_x += self.a_x * dt 
+        self.v_y += self.a_y * dt 
         self.x += self.v_x * dt + 0.5 * self.a_x * dt ** 2
         self.y += self.v_y * dt + 0.5 * self.a_y * dt ** 2
 
@@ -53,7 +55,7 @@ class System():
         Y = np.random.uniform(-self.box.y/2, self.box.y/2, self.n)
         self.lst_beads=[Bead(x,y) for x,y in zip(X,Y)]
     
-    def pair_interaction(self,bead1,bead2):
+    def pair_interaction(self, bead1, bead2):
         dx = bead1.x - bead2.x
         dy = bead1.y - bead2.y
         dx, dy = self.box.periodic_correct(dx, dy)
@@ -67,7 +69,8 @@ class System():
             b.a_x = 0
             b.a_y = 0
         for pt in self.dbl_list:
-            self.pair_interaction(self.lst_beads[pt[0]],self.lst_beads[pt[1]])
+            self.pair_interaction(self.lst_beads[pt[0]], self.lst_beads[pt[1]])
+        thermostat(self.lst_beads)
         for b in self.lst_beads:
             b.move_bead(self.dt)  
             b.x, b.y = self.box.periodic_correct(b.x, b.y)  
@@ -86,20 +89,35 @@ def show_beads(sys, namefile='Pic', num_pic=-1):
         namefile = namefile + '{:04d}'.format(num_pic) + '.jpg'
         plt.savefig(namefile)  
 
-def animation(delay=10, loop=0, namegif='animatedGIF'):
+def animation(delay=10, loop=0, namegif='animatedGIF1'):
     #Gifanimationmaker should be installed: sudo apt-get install imagemagick
-    os.system(f'convert -delay {delay} -loop {loop} *.jpeg {namegif}.gif')
+    os.system(f'convert -delay {delay} -loop {loop} *.jpg {namegif}.gif')
     os.system('rm *.jpg') 
-       
+
+def thermostat(list_beads):
+    sum_vx = 0
+    sum_vy = 0
+    for bead in list_beads:
+        sum_vx += bead.v_x**2
+        sum_vy += bead.v_y**2
+    sum_vx, sum_vy = np.sqrt(sum_vx/len(list_beads)), np.sqrt(sum_vy/len(list_beads))
+    if (sum_vx != 0) & (sum_vy != 0):
+        for bead in list_beads:
+            bead.v_x = bead.v_x / sum_vx
+            bead.v_y = bead.v_y / sum_vx
+    
+           
       
 if __name__ == '__main__':
     box = Box(5,5)    
     ff = ForceField(r_c = 1.0, att = 1.0)
-    sys = System(dt = 0.1, n = 75, box=box, ff=ff)
+    sys = System(dt = 0.02, n = 75, box=box, ff=ff)
     sys.initial_configuration()
     show_beads(sys, num_pic=0)
-    for n_step in range(100):
+    for n_step in range(1000):
         sys.step()
-        show_beads(sys, num_pic=n_step+1)
+        print(sys.lst_beads[0].v_x)
+        if n_step % 10 == 0:
+            show_beads(sys, num_pic=n_step+1)
     animation()
     
